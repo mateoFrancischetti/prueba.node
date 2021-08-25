cc2c072040f9ec032e4e845203b498b2a613fb9e
mateoFrancischetti <mateofrancischetti@gmail.com>
2021-08-25 13:35:14 -0300

    modifique logica.js

---
+++ logica.js
@@ -0,0 +1,29 @@
+// Primer codigo
+let edad = 18;
+ let altura = 175;
+
+
+console.log(edad>= 12 && altura>= 130);
+
+// Segundo código
+
+let suficienteLuz = false;
+let objetoSeMueveRapidamente = true;
+let flash = true;
+
+console.log(suficienteLuz === false || objetoSeMueveRapidamente === true);
+
+// Tercer código
+
+let notaParcial1 = 7; 
+let notaParcial2 = 5;
+let notaFinal = 4;
+let promedioParciales = (notaParcial1 >= 7 && notaParcial2 >7)
+console.log( promedioParciales || notaFinal >= 4);
+
+// Cuarto código 
+
+let tarea = true;
+let PianoDeMemoria = false; 
+
+console.log(tarea && PianoDeMemoria);
